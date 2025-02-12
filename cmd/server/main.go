ddddbe6de3b6bf0375316b6155ee25619c60609b
Dmitry Yggdrasil <richard.aliyev.00@gmail.com>
2025-02-12 18:40:09 +0300

    creating rest-api

---
+++ cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func Run() error {
+	fmt.Println("Starting up our application")
+	return nil
+}
+
+func main() {
+	fmt.Println("Go start")
+
+	if err := Run(); err != nil {
+		fmt.Println(err)
+	}
+}
